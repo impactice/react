993057e7b7c99608ca41a01ccb01aad49d215759
impactice <107308111+impactice@users.noreply.github.com>
2025-04-02 14:08:47 +0900

    Add files via upload

--- component/index.js
+++ component/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-
+import CommentList from './chapter_05/CommentList';
 const root = document.getElementById("root");
-
-ReactDOM.createRoot(root).render(<App />);
+ReactDOM.createRoot(root).render(<CommentList />);
